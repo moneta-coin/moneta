bb17e32215b78d2659fa9e3b178fb64ea1cfbbbb
imak81 <imak81@mail.ru>
2015-09-07 00:07:26 +0400

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1736,7 +1736,7 @@ bool BindListenPort(const CService &addrBind, string& strError)
     {
         int nErr = WSAGetLastError();
         if (nErr == WSAEADDRINUSE)
-            strError = strprintf(_("Unable to bind to %s on this computer. Moneta is probably already running."), addrBind.ToString().c_str());
+            strError = strprintf(_("Unable to bind to %s on this computer. Amuse is probably already running."), addrBind.ToString().c_str());
         else
             strError = strprintf(_("Unable to bind to %s on this computer (bind returned error %d, %s)"), addrBind.ToString().c_str(), nErr, strerror(nErr));
         printf("%s\n", strError.c_str());
@@ -1759,7 +1759,6 @@ bool BindListenPort(const CService &addrBind, string& strError)
 
     return true;
 }
-
 void static Discover()
 {
     if (!fDiscover)
